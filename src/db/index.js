1595c27d0e06f180e884d5d5fa79340d64230c3a
padakitanmay <padakitanmay@gmail.com>
2024-04-05 16:57:22 +0530

    mongo connected

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `Mongo DB connected at host: ${
+                (await connectionInstance).connection.host
+            }`
+        );
+    } catch (error) {
+        console.log("Mongo DB Connection failed due to error", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
