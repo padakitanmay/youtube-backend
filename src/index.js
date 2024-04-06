c80efba932c04c54f41960aad9cc91e476ff5163
padakitanmay <padakitanmay@gmail.com>
2024-04-06 11:45:28 +0530

    express was configured and some libraries are insatalled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const port = process.env.PORT || 8000;
 
 dotenv.config({ path: "./env" });
-connectDB();
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log("Server running on port: ", port);
+        });
+    })
+    .catch((err) => {
+        console.log("DB failed", err);
+    });
